fd314a454a4a6d1339c49dde813581adfd67bcf8
Adrian Rangel <adrian.rangel@gmail.com>
2016-01-25 18:53:38 -0600

    passing execute method

--- first_example/lib/sum.rb
+++ first_example/lib/sum.rb
@@ -1,2 +1,10 @@
 class Sum
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end
+
+  def execute
+    @a + @b
+  end
 end
--- first_example/spec/sum_spec.rb
+++ first_example/spec/sum_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 require 'sum'
 
 describe Sum do
-  it 'sums two positive values' do
+  it 'sums 3 and 5' do
     sum = Sum.new(3, 5)
+    expect(sum.execute).to eql(8)
+  end
+
+  it 'sums 6 and 9' do
+    sum = Sum.new(6, 9)
+    expect(sum.execute).to eql(15)
+  end
+
+  it 'sums -1 and 9' do
+    sum = Sum.new(-1, 9)
     expect(sum.execute).to eql(8)
   end
 end
