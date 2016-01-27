4a79b5a54dd1c94af693f5d7e3e18de3836d2904
Adrian Rangel <adrian.rangel@gmail.com>
2016-01-26 23:24:22 -0600

    custom delimeters

--- calculator_kata/lib/string_calculator.rb
+++ calculator_kata/lib/string_calculator.rb
@@ -1,6 +1,9 @@
 class StringCalculator
   def self.add(string_numbers)
     return 0 if string_numbers.empty?
-    string_numbers.split(/(,|\n)/).map(&:to_i).reduce(&:+)
+    delimiters = /(,|\n)/
+    delimiters = string_numbers[2] if string_numbers.start_with?('//')
+
+    string_numbers.split(delimiters).map(&:to_i).reduce(&:+)
   end
 end
--- calculator_kata/spec/string_calculator_spec.rb
+++ calculator_kata/spec/string_calculator_spec.rb
@@ -22,5 +22,9 @@ describe StringCalculator do
     it 'will handle new lines as delimiters' do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it 'will handle custom delimiters' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
