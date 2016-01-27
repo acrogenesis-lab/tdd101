0a3235ec4a36d09d31fd5c19e1faaf8f44b9fe77
Adrian Rangel <adrian.rangel@gmail.com>
2016-01-26 23:10:34 -0600

    it adds

--- calculator_kata/lib/string_calculator.rb
+++ calculator_kata/lib/string_calculator.rb
@@ -1,3 +1,6 @@
 class StringCalculator
-
+  def self.add(string_numbers)
+    return 0 if string_numbers.empty?
+    string_numbers.split(',').map(&:to_i).reduce(&:+)
+  end
 end
--- calculator_kata/spec/string_calculator_spec.rb
+++ calculator_kata/spec/string_calculator_spec.rb
@@ -1,2 +1,22 @@
 require 'spec_helper'
 require 'string_calculator'
+
+describe StringCalculator do
+  describe '.add' do
+    it 'will return 0 for an empty string' do
+      expect(StringCalculator.add('')).to eq(0)
+    end
+
+    it 'will return 1 for "1"' do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
+
+    it 'will return 3 for "1,2"' do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
+
+    it 'will return 45for "15,30"' do
+      expect(StringCalculator.add('15,30')).to eq(45)
+    end
+  end
+end
